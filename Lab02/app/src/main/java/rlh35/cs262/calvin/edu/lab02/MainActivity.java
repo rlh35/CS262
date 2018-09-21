58a6f69604491735dd1387cb59b23a17078c9a69
Ryan Holwerda <rlh35@students.calvin.edu>
2018-09-21 16:59:29 -0400

    Lab01 and Lab02

---
+++ Lab02/app/src/main/java/rlh35/cs262/calvin/edu/lab02/MainActivity.java
@@ -0,0 +1,36 @@
+package rlh35.cs262.calvin.edu.lab02;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/* CS262
+ * Lab02
+ * Ryan Holwerda (rlh35) and Ryan Johnson (rkj4)
+ */
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
